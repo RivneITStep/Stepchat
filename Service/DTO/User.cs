1339158d3b19513df7eee4bd7658adb5fcad34ba
PavlMais <pavlomajstruk@gmail.com>
2020-04-23 18:51:10 +0300

    add methods

---
+++ Service/DTO/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.DTO
+{
+    [DataContract]
+    public class User
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string FirstName { get; set; }
+        [DataMember]
+        public string LastName { get; set; }
+        [DataMember]
+        public string Bio { get; set; }
+        [DataMember]
+        public string PhotoPath { get; set; }
+        [DataMember]
+        public string Login { get; set; }
+        [DataMember]
+        public bool IsOnline { get; set; }
+        [DataMember]
+        public DateTime LastOnlineDate { get; set; }
+    }
+}
---
+++ Service/Service.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Service.DTO;
+
+using DAL;
+using AutoMapper;
+
+namespace Service
+{
+    public class Service : IService
+    {
+        private static DALClass dal = new DALClass();
+        private static IMapper mapper;
+
+        static Service() // Initialization
+        {
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<DAL.User, DTO.User>();
+                cfg.CreateMap<DTO.User, DAL.User>();
+
+                cfg.CreateMap<DAL.Chat, DTO.Chat>();
+                cfg.CreateMap<DTO.Chat, DAL.Chat>();
+
+                cfg.CreateMap<DAL.Message, DTO.Message>();
+                cfg.CreateMap<DTO.Message, DAL.Message>();
+            });
+            mapper = config.CreateMapper();
+        }
+
+        public Result AddUserToChat(int userId, int chatId)
+        {
+            return Result.WithError(ResultError.NotImplemented);
+        }
+
+        public Result ChangePassword(string newPassword)
+        {
+            return Result.WithError(ResultError.NotImplemented);
+        }
+
+        public Result CreateChat(string name)
+        {
+            return Result.WithError(ResultError.NotImplemented);
+        }
+
+        public Result CreatePrivateChat(int userId)
+        {
+            return Result.WithError(ResultError.NotImplemented);
+        }
+
+        public Result EditName(string newFirstname, string newLastname, string newBio)
+        {
+            return Result.WithError(ResultError.NotImplemented);
+        }
+
+        public Result<IEnumerable<DTO.Message>> GetChatHistory(int chatId)
+        {
+            return Result<IEnumerable<DTO.Message>>.WithError(ResultError.NotImplemented);
+        }
+
+        public Result<IEnumerable<DTO.Chat>> GetChats()
+        {
+            return Result<IEnumerable<DTO.Chat>>.WithError(ResultError.NotImplemented);
+        }
+
+        public Result JoinToChat(int chatId)
+        {
+            return Result.WithError(ResultError.NotImplemented);
+        }
+
+        public Result LeaveFromChat(int chatId)
+        {
+            return Result.WithError(ResultError.NotImplemented);
+        }
+
+        public Result Login(string login, string password)
+        {
+            dal.Login(login, password);
+
+            return Result.OK;
+        }
+        public Result Register(DTO.User user)
+        {
+            dal.Register(mapper.Map<DAL.User>(user));
+            return Result.OK;
+            
+        }
+
+        public Result<IEnumerable<DTO.Chat>> SearchChats(string query)
+        {
+            return Result<IEnumerable<DTO.Chat>>.WithError(ResultError.NotImplemented);
+        }
+
+        public Result<IEnumerable<DTO.User>> SearchUsers(string query)
+        {
+            return Result<IEnumerable<DTO.User>>.WithError(ResultError.NotImplemented);
+        }
+
+        public Result SendMessage(int chatId, string message)
+        {
+            return Result.WithError(ResultError.NotImplemented);
+        }
+    }
+}
