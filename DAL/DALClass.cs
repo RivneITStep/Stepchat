8cda908bc4daebb1374279ef87829fe55bb2a3c7
Dima Kostiuk <DimaKostiuk@outlook.com>
2020-04-23 11:48:00 +0300

    Add metods Login and Register

---
+++ DAL/DALClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DALClass
+    {
+        ChatDBModel context;
+
+        public DALClass()
+        {
+            context = new ChatDBModel();
+        }
+
+        public bool Login (string login, string hashPassword)
+        {
+            User user = context.Users.FirstOrDefault(u => u.Login == login);
+
+            if (user == null)
+                return false;
+
+            if (user.Password == hashPassword)
+                return true;
+            else
+                return false;
+        }
+        public void Register(User user)
+        {
+            var existing = context.Users.FirstOrDefault(u => u.Login == user.Login);
+
+            if (existing != null) return;
+
+            context.Users.Add(user);
+            context.SaveChanges();
+        }
+    }
+}
