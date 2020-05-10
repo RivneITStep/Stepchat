071dd995995ef929dbbf45ada41b05082e55b465
nazar pavliuk <hesab.k18@gmail.com>
2020-05-10 14:33:08 +0300

    update service

--- Service/DTO/User.cs
+++ Service/DTO/User.cs
@@ -23,12 +23,13 @@ namespace Service.DTO
         [DataMember]
         public string Login { get; set; }
         [DataMember]
+        public string Password { get; set; }
+        [DataMember]
         public bool IsOnline { get; set; }
         [DataMember]
         public DateTime LastOnlineDate { get; set; }
         [DataMember]
         public string Email { get; set; }
-        [DataMember]
-        public string Password { get; set; }
+      
     }
 }
